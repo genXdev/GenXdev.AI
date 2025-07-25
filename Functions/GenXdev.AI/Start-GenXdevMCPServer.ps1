bf11a67909a19c175db2d3dee5c885dacfb23d3d
René Vaessen <rene.vaessen@gmail.com>
2025-07-25 02:12:32 +0200

    Release 1.216.2025

--- Functions/GenXdev.AI/Start-GenXdevMCPServer.ps1
+++ Functions/GenXdev.AI/Start-GenXdevMCPServer.ps1
@@ -99,7 +99,7 @@ function Start-GenXdevMCPServer {
                     }
                     serverInfo      = @{
                         name    = 'GenXdev-PowerShell-MCP-Server'
-                        version = '1.214.2025'
+                        version = '1.216.2025'
                     }
                 }
             }
@@ -197,7 +197,7 @@ function Start-GenXdevMCPServer {
                             }
                             serverInfo      = @{
                                 name    = 'GenXdev-PowerShell-MCP-Server'
-                                version = '1.214.2025'
+                                version = '1.216.2025'
                             }
                         }
                     }
