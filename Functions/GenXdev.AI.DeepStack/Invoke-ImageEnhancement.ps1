47ab8806c751155548c2e9d8caec3a156d14cf72
René Vaessen <rene.vaessen@gmail.com>
2025-09-16 15:09:04 +0200

    Release 1.270.2025

--- Functions/GenXdev.AI.DeepStack/Invoke-ImageEnhancement.ps1
+++ Functions/GenXdev.AI.DeepStack/Invoke-ImageEnhancement.ps1
@@ -2,7 +2,7 @@
 Part of PowerShell module : GenXdev.AI.DeepStack
 Original cmdlet filename  : Invoke-ImageEnhancement.ps1
 Original author           : René Vaessen / GenXdev
-Version                   : 1.268.2025
+Version                   : 1.270.2025
 ################################################################################
 MIT License
 
